e06d7ca6d8fc6dff0df5519a4840bd8f548d4e53
Alexis Lê-Quôc <alq@datadoghq.com>
2012-07-05 15:28:21 -0400

    Minimal prettification of the message title.

--- dogknife.rb
+++ dogknife.rb
@@ -11,14 +11,16 @@ begin
   Chef::Application::Knife.new.run
 rescue SystemExit
   duration = Time.now.to_i - start
+  # cheap identification, ideally, pull this from .chef/knife.rb
   who = %x[whoami].strip
   # skip help
   if ARGV.include?("help") || ARGV.include?("--help")
     puts "Ignoring help calls"
   else
+    # will be replaced by a lookup in .chef/knife.rb
     dog = Dogapi::Client.new(ENV['DATADOG_KEY'])
     dog.emit_event(Dogapi::Event.new("#{ARGV.join(' ')}",
-                                     :msg_title       => "#{who} ran knife #{ARGV.join(' ')} in #{duration} seconds",
+                                     :msg_title       => "#{who} ran: knife #{ARGV.join(' ')} in #{duration} seconds",
                                      :aggregation_key => "#{who}-knife",
                                      :alert_type      => "info",
                                      :source_type_name => "chef",
