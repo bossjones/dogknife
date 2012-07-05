afa75968aee511425d6c6916d73d0ea69a4ca563
Alexis Lê-Quôc <alq@datadoghq.com>
2012-07-05 15:23:26 -0400

    Very first cut of the code. Naive, but works.

---
+++ dogknife.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'chef/application/knife'
+require 'dogapi'
+
+# First approach, wrap the call
+start = Time.now.to_i
+args = ARGV
+
+begin
+  Chef::Application::Knife.new.run
+rescue SystemExit
+  duration = Time.now.to_i - start
+  who = %x[whoami].strip
+  # skip help
+  if ARGV.include?("help") || ARGV.include?("--help")
+    puts "Ignoring help calls"
+  else
+    dog = Dogapi::Client.new(ENV['DATADOG_KEY'])
+    dog.emit_event(Dogapi::Event.new("#{ARGV.join(' ')}",
+                                     :msg_title       => "#{who} ran knife #{ARGV.join(' ')} in #{duration} seconds",
+                                     :aggregation_key => "#{who}-knife",
+                                     :alert_type      => "info",
+                                     :source_type_name => "chef",
+                                     :tags            => ["knife"]
+                                     ))
+  end
+end
+exit 0
+
