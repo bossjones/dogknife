63a4ee7c0dca6d9a11a3e79ff35b71f20b3c42a6
Alexis Lê-Quôc <alq@datadoghq.com>
2012-07-07 23:22:26 -0400

    Able to get logging, but no stdout/stderr yet.

--- dogknife.rb
+++ dogknife.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'chef/config'
 require 'chef/knife'
+require 'chef/log'
 require 'chef/application/knife'
 require 'dogapi'
 
@@ -12,32 +13,55 @@ $real_run = Chef::Knife.method(:run)
 class Chef
   class Knife
     def self.run(args, options={})
+      # Capture logging
+      lrd, lwr = IO.pipe
+      dd_logger = Logger.new(lwr)
+      Chef::Log.loggers << dd_logger
+
+      # Measure duration
       start = Time.now.to_i
+      # Call run
       $real_run.call(args, options)
+
       duration = Time.now.to_i - start
-      # skip help
+
+      # Don't capture help messages skip help
       unless args.include?("help") && args.include?("--help")
 
         # Look up some properties
+        who = %x[whoami].strip
         if Chef::Config.has_key?("datadog_user")
           who = Chef::Config.datadog_user
-        else
-          who = %x[whoami].strip
-        end
-        if Chef::Config.has_key?("datadog_api_key")
-          dog = Dogapi::Client.new(Chef::Config.datadog_api_key)
-        else
-          dog = Dogapi::Client.new(ENV['DATADOG_KEY'])
         end
 
+        dog = nil
+        if Chef::Config.has_key?("datadog_api_key")
+          dog = Dogapi::Client.new(Chef::Config.datadog_api_key)
+        elsif ENV.has_key?("DATADOG_KEY")
+          dog = Dogapi::Client.new(ENV['DATADOG_KEY'])
+        else
+          self.msg("Missing datadog_api_key in knife configuration")
+        end
+        
         # Emit event to Datadog
-        dog.emit_event(Dogapi::Event.new("#{args.join(' ')}",
-                                         :msg_title       => "#{who} ran: knife #{args.join(' ')} in #{duration} seconds",
-                                         :aggregation_key => "#{who}-knife",
-                                         :alert_type      => "info",
-                                         :source_type_name => "chef",
-                                         :tags            => ["knife"]
-                                         ))
+        unless dog.nil?
+          begin
+            lwr.close_write
+            logged = lrd.read
+            body = "Nothing logged"
+            body = "Knife logged\n@@@#{logged}@@@" if logged.length > 0
+            dog.emit_event(Dogapi::Event.new(body,
+                                             :msg_title       => "#{who} ran: knife #{args.join(' ')} in #{duration} second(s)",
+                                             :event_type      => "config_management.command",
+                                             :aggregation_key => "#{who}-knife",
+                                             :alert_type      => "info",
+                                             :source_type_name => "chef",
+                                             :tags            => ["knife"]
+                                             ))
+          rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT => e
+            ui.error("Could not send data to Datadog")
+          end
+        end
       end
     end
   end
